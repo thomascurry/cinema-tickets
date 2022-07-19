dda980bc6c23357a8ffc6f56b3d43535b91a7a0d
Thomas Curry <thomasjcurry@hotmail.com>
2022-07-19 19:35:49 +0100

    add initial router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const Express = require("express");
 
 const app = Express();
 
+app.set("base", "/api");
+
+app.use("tickets", tickets);
+
 app.listen(3000, () => {
   console.log("== APP STARTED ON PORT 3000 ==");
 });
