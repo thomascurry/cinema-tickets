e18e6eac4b0e230498b4710cbab6c90c4ca3030c
Thomas <thomas.curry@engineering.digital.dwp.gov.uk>
2022-07-21 16:10:31 +0100

    Add failure tests for controller

--- src/pairtest/controllers/tickets.controller.js
+++ src/pairtest/controllers/tickets.controller.js
@@ -1,6 +1,14 @@
 import TicketService from "../services/TicketService"
 
+const ticketService = new TicketService();
+
 export const purchaseTickets = async (req, res, next) => {
-    
-    return res.status(200)
+    try {
+        const result = ticketService.purchaseTickets()
+
+        return res.status(200)
+    } catch(error) {
+        console.error('An error has occured')
+        next(error);
+    }
 }
--- test/unit/pairtest/controllers/tickets.controller.test.js
+++ test/unit/pairtest/controllers/tickets.controller.test.js
@@ -1,14 +1,17 @@
+import { jest } from '@jest/globals';
 import { purchaseTickets } from "../../../../src/pairtest/controllers/tickets.controller";
-import {jest} from '@jest/globals';
+import TicketService from '../../../../src/pairtest/services/TicketService'
+jest.mock('../../../../src/pairtest/services/TicketService')
 
 afterEach(() => {
     jest.clearAllMocks();
     jest.resetAllMocks();
     jest.restoreAllMocks();
-  });
+});
 
 describe("purchaseTickets", () => {
     it("should return a 200 when a purchase is completed", async () => {
+        // SETUP
         const fakeReq = {}
 
         const fakeRes = {
@@ -17,8 +20,43 @@ describe("purchaseTickets", () => {
 
         const fakeNext = jest.fn()
 
+        const purchaseTicketsSpy = jest
+        .spyOn(TicketService.prototype, 'purchaseTickets')
+        .mockImplementationOnce(() => {});
+
+        // CALL
         await purchaseTickets(fakeReq, fakeRes, fakeNext)
+        
+        // EXPECT
+        expect(purchaseTicketsSpy).toHaveBeenCalled();
+    
         expect(fakeRes.status).toHaveBeenCalledWith(200)
         expect(fakeNext).not.toHaveBeenCalled();
+    })
+
+    it("should call next function when a downstream error is thrown", async () => {
+        // SETUP
+        const fakeReq = {}
+
+        const fakeRes = {
+            status: jest.fn()
+        }
+
+        const fakeNext = jest.fn()
+
+        const purchaseTicketsSpy = jest
+        .spyOn(TicketService.prototype, 'purchaseTickets')
+        .mockImplementationOnce(() => {
+            throw new Error('some error')
+        });
+  
+        // CALL
+        await purchaseTickets(fakeReq, fakeRes, fakeNext)
+
+        // EXPECT
+        expect(purchaseTicketsSpy).toHaveBeenCalled();
+
+        expect(fakeNext).toHaveBeenCalled();
+        expect(fakeRes.status).not.toHaveBeenCalled();
     })
 })
